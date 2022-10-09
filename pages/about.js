f8c86105849ecc9771276e49b6db8faf5fdc45ed
ifty03 <ashikulislamb84@gmail.com>
2022-10-09 14:26:29 +0600

    write module.css with next.js

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,42 @@
 import React from "react";
+import styles from "../styles/about.module.css";
 
 const About = () => {
   return (
-    <div>
-      <h2>Hello ashik.. why you learn Next.js</h2>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Ashiks world</a>
+        </h1>
+
+        <p className={styles.description}>Get started With ashikul islam </p>
+
+        <div className={styles.grid}>
+          <a
+            href="https://nextjs.org/docs"
+            className={`${styles.card} ${styles.bgr}`}
+          >
+            <h2>Students &rarr;</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus
+              obcaecati perspiciatis quis? Voluptatum aspernatur nostrum nisi,
+              autem sequi laudantium quas!
+            </p>
+          </a>
+
+          <a
+            href="https://nextjs.org/learn"
+            className={`${styles.card} ${styles.bgb}`}
+          >
+            <h2>Jobs &rarr;</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus
+              obcaecati perspiciatis quis? Voluptatum aspernatur nostrum nisi,
+              autem sequi laudantium quas!
+            </p>
+          </a>
+        </div>
+      </main>
     </div>
   );
 };
