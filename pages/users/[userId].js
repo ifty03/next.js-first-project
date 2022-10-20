b5eefbbfce82bc7663c97590a5b8ba96a20a1c15
ifty03 <ashikulislamb84@gmail.com>
2022-10-20 21:46:42 +0600

    nothing to commit

--- pages/users/[userId].js
+++ pages/users/[userId].js
@@ -2,7 +2,7 @@ function singleUser({ user }) {
   return (
     <div style={{ margin: "100px" }}>
       <h2>User Id:{user.id}</h2>
-      <h2>{user.name}</h2>
+      <h2>User Name: {user.name}</h2>
       <h4>User Email: {user.email}</h4>
       <p>City: {user.address.city}</p>
     </div>
